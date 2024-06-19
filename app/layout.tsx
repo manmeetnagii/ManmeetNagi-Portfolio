f7548159b7e8f4d470278b64a2678e42b3e93747
Manmeet <manmeetnagiii@gmail.com>
2024-06-19 10:38:52 +0530

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <FloatingNavDemo />
         {children}
